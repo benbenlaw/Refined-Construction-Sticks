cfb281ad2a1a92ba25343cb8b91efd697d6a94b6
benbenlaw <benlawrenceben@hotmail.co.uk>
2026-05-15 13:28:28 +0100

    1.2.1

--- src/main/java/com/benbenlaw/refinedsticks/event/ServerEvents.java
+++ src/main/java/com/benbenlaw/refinedsticks/event/ServerEvents.java
@@ -72,8 +72,7 @@ public class ServerEvents {
 
         if (!(block instanceof AbstractControllerBlock<?>)) return;
         if (!(stack.getItem() instanceof ItemStickBasic)) return;
-        if (player.level().isClientSide) return;
-        if (!Minecraft.getInstance().player.isCrouching()) return;
+        if (!player.isCrouching()) return;
 
         UseOnContext ctx = new UseOnContext(
                 player,
